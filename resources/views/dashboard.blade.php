d5231b6d56fa17fe0e36fb36bb5fa60bd2f19110
Muhd Jamil Husni <maintanance1991@gmail.com>
2022-11-29 01:16:18 +0800

    dashboard first <li> adjust

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -59,6 +59,11 @@
         border: 3px solid #fff;
         border-radius: 50%;
         }
+
+        a {
+            text-decoration: none !important;
+            color : white !important;
+        }
     </style>
 </head>
 <body>
@@ -110,12 +115,18 @@
         <li class="navList__heading">documents<i class="far fa-file-alt"></i></li>
 
           <li>
-          <div class="navList__subheading row row--align-v-center">
-            <span class="navList__subheading-icon">
-                <i class="fas fa-briefcase-medical" style="position : relative; top : 10px;"></i>
+          <a href="{{route('dash')}}">
+          <div class="ownli">
+            <span class="navList__subheading-icon" style="padding-top : 15px; margin-left : 27px;">
+                <span style="position : relative; top : 3px; margin-right : 10px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-speedometer" viewBox="0 0 16 16">
+                <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+                <path fill-rule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z"/>
+                </svg>
             </span>
-            <span class="navList__subheading-title" style="position : relative; top : -7px;">insurance</span>
+            </span>
+            <span class="navList__subheading-title" style="position : relative; top : -16px; margin-left : 44px;">Dashboard</span>
           </div>
+          </a>
           
         </li>
 
